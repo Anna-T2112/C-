8d2b6ce1abb71b61a5b6d483a6b0301414b6e166
AnnaT <augenblick21@mail.ru>
2022-10-27 20:38:12 +0300

    done task23

---
+++ seminar3/task23/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 23 Напишите программу, которая принимает на вход 
+// число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.WriteLine("Введите число N:");
+var n = int.Parse(Console.ReadLine());
+var result = Pow3(n);
+Console.WriteLine(result);
+
+static string Pow3(int n)
+{
+    var result = string.Empty;
+    for (int i = 1; i <= n; i++)
+        result += i == n ? Math.Pow(i, 3).ToString() : Math.Pow(i, 3).ToString() + ',';
+
+    return result;
+}
