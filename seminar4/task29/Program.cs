883d942df45fbeb734c70d2f39a3d32cee7dc988
AnnaT <augenblick21@mail.ru>
2022-11-10 20:05:27 +0300

    done task 29

---
+++ seminar4/task29/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int[] numbers = new int[8];
+Console.WriteLine("Введите восемь цифр:");
+var array = new long[8];
+for (var i = 0; i < 8; i++)
+{
+    array[i] = long.Parse(Console.ReadLine());
+}
+Console.WriteLine($"[{string.Join(",", array)}]");
