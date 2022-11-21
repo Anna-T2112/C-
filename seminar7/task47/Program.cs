12443a405d7a0c817176f47a3c9c2a40fc57a8f9
AnnaT <augenblick21@mail.ru>
2022-11-21 20:14:45 +0300

    done task 47

---
+++ seminar7/task47/Program.cs
@@ -0,0 +1,37 @@
+﻿//Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+double[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    double[,] result = new double[m, n];
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            double value = new Random().NextDouble() * (maxValue - minValue) + minValue;
+            result[i, j] = value;
+        }
+    }
+
+    return result;
+}
+
+void PrintArray(double[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]:0.0} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Введите кол-во строк: ");
+int rows = int.Parse(Console.ReadLine()!);
+Console.Write("Введите кол-во стобцов: ");
+int cols = int.Parse(Console.ReadLine()!);
+
+double[,] array = GetArray(rows, cols, -10, 10);
+PrintArray(array);
